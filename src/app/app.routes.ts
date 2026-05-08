ad338e0797a45451c8cde330a769d045544e640f
Carlos Vilches <carlesvilsi@gmail.com>
2026-05-08 12:59:03 +0200

    Afegit Lazy Loading (EX3)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { ListaTaladrosComponent } from './components/lista-taladro/lista-taladro.component';
 import { FormulariCercaComponent } from './components/formulari-cerca/formulari-cerca.component';
-import { PreferitsPanelComponent } from './components/preferits-panel/preferits-panel.component';
 import { DetallComponent } from './components/detall/detall.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guards/auth.guard';
@@ -11,7 +10,7 @@ export const routes: Routes = [
 
     { path: 'taladros', component: ListaTaladrosComponent, title: 'LListat de Taladros'},
     { path: 'cerca', component: FormulariCercaComponent, title: 'Cerca de Taladros'},
-    { path: 'preferits', component: PreferitsPanelComponent, title: 'Els meus preferits', canActivate: [authGuard]},
+    { path: 'preferits', loadComponent: () => import ('./components/preferits-panel/preferits-panel.component').then(m => m.PreferitsPanelComponent), title: 'Els meus preferits', canActivate: [authGuard]},
     { path: 'detall/:id', component: DetallComponent, title: 'Detall'},
     { path: 'login', component: LoginComponent, title: 'Inicia Sessió'},
 
